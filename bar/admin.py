4fb74618454eff038006f94aa0fcd34421652235
naomi wilson <naomiwilson699@gmail.com>
2026-01-04 15:23:31 +0300

    chore: initialize pub manager backend project

---
+++ bar/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import DrinkCategory, Drink
+
+admin.site.register(DrinkCategory)
+admin.site.register(Drink)
---
+++ pub_manager/git init.md
@@ -0,0 +1,3 @@
+git init
+git branch -M main
+git remote add origin https://github.com/NaomiWilsonMushi/pub-manager-backend.git
